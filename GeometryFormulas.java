801dff5f44a46e7021650710ae9affac73353500
zowow <79232193+zowow@users.noreply.github.com>
2021-03-16 18:46:33 +0800

    Initial Commit

---
+++ GeometryFormulas.java
@@ -0,0 +1,15 @@
+public class GeometryFormulas{
+
+    
+    public static void main(String[] args) {
+        
+         Formulas fmla = new Formulas();
+
+    }
+    
+}
+
+class Formulas{
+    
+    
+}
